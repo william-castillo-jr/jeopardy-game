ef817168dbfb088796db54d5a467d8dc5d54ede6
William Castillo <william.castillo.jr@outlook.com>
2025-05-09 21:22:03 -0700

    Added modal window functionality when a button is clicked

--- jeopardy-game/src/components/GameBoard/GameBoard.jsx
+++ jeopardy-game/src/components/GameBoard/GameBoard.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import './GameBoard.css';
-import _questions from "./../../data/questions.json";
+import React, { useState } from 'react';
+import _questions from "./../../../public/data/questions.json";
+import QuestionModal from '../QuestionModal/QuestionModal.jsx'
+import Card from './../Card/Card.jsx'
+import './GameBoard.css'
 
 function GameBoard() {
-    return (
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+  const handleQuestionClick = (question) => {
+    setSelectedQuestion(question);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedQuestion(null);
+  };
+
+  return (
     <div className="categories">
-        {_questions.map((category, index) => (
-            <div key={index} className="category"> 
-                <h2>{category.category}</h2>
-                {category.questions.map((question, index) => (
-                <button key={index} className="question-button">
-                    ${question.value}
-                </button>
-            ))}
+      {_questions.map((category, index) => (
+        <div key={index} className="category">
+          <h2>{category.category}</h2>
+          {category.questions.map((question, idx) => (
+            <Card
+              key={idx}
+              question={question}
+              onClick={() => handleQuestionClick(question)}
+            >
+              ${question.value}
+            </Card>
+          ))}
         </div>
-        ))}
+      ))}
+      {isModalOpen && selectedQuestion && (
+        <QuestionModal
+          question={selectedQuestion}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
-    )
+  );
 }
 
 export default GameBoard;
