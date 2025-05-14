3497ed4d0b2dadf82978931a705bdccc14c11dd8
William Castillo <william.castillo.jr@outlook.com>
2025-05-14 15:38:10 -0700

    Include card answer and queston
    Create styling
    
    Add new Answer component

--- jeopardy-game/src/components/QuestionModal/QuestionModal.jsx
+++ jeopardy-game/src/components/QuestionModal/QuestionModal.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
 import './QuestionModal.css'; 
+import Answer from './Answer.jsx'
+import { useState } from 'react'
 
 function QuestionModal({ question, onClose }) {
+  const [showContents, setShowContents] = useState(true);
+
+  const handleToggleContents = () => setShowContents(!showContents);
+
     return (
-        <div className="modal-overlay" onClick={onClose}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <h2>{question.question}</h2>
-            <p><strong>Answer:</strong> {question.answer}</p>
-            <button onClick={onClose}>Close</button>
-          </div>
+        <div className="modal-overlay" onClick={onClose}> 
+          <div className="modal-content" onClick={(e) => e.stopPropagation()} >
+        {showContents ? (
+          <>
+            <p className="question-category">Question Category</p>
+            <button className="card-cost">Card button with cost</button>
+            <p>{question.answer}</p>
+            <button onClick={onClose}>Close</button>    
+            <button className="answer" onClick={handleToggleContents}>Show Answer</button>
+          </> ) : <Answer answer={question.question} onClose={onClose}/>}
         </div>
+        
+      </div>
     );
 }
 
