c974d3fdf7438b3ad8a2f9a2a1ddb822952fb649
William Castillo <william.castillo.jr@outlook.com>
2025-05-14 15:34:42 -0700

    Create a container for the gameboard and scoreboard

--- jeopardy-game/src/App.jsx
+++ jeopardy-game/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import GameBoard from './components/GameBoard/GameBoard.jsx'
+import Scoreboard from './components/Scoreboard/Scoreboard.jsx'
 
 function App() {
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+  const handleQuestionClick = (question) => {
+    setSelectedQuestion(question);
+  }
+  const handleBackToBoard = () => {
+    setSelectedQuestion(null);
+  }
+
   return (
-    <>
-      <GameBoard />
-    </>
-  )
+    <div className="app-container">
+      <GameBoard selectedQuestion={selectedQuestion}
+        onQuestionClick={handleQuestionClick}
+        onClose={handleBackToBoard}
+      />
+      {!selectedQuestion && <Scoreboard />}
+    </div>
+  );
 }
 
 export default App
