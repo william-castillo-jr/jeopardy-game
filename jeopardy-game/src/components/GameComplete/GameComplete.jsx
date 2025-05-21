32d4ad4d0907f5b12bc3a5cd01c861301c11b002
William Castillo <william.castillo.jr@outlook.com>
2025-05-21 11:42:01 -0700

    Create animation for opening modal

--- jeopardy-game/src/components/GameComplete/GameComplete.jsx
+++ jeopardy-game/src/components/GameComplete/GameComplete.jsx
@@ -1,11 +1,15 @@
 import './GameComplete.css'
+import Grow from '@mui/material/Grow';
 
-function GameComplete() {
+function GameComplete({ onReturnToGame }) {
     return (
-        <div className="modal-overlay"> 
+        <div className="modal-overlay">
+            <Grow in={true} timeout={700}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()} >
             <p className="question-category">Question Category</p>
+            <button onClick={onReturnToGame}>Return to Gameboard</button>
         </div>
+        </Grow>
       </div>
     )
 }
